ec9ff256bfef4e041ffe295f39699775bf425cde
PARJOAC <133354750+PARJOAC@users.noreply.github.com>
2024-10-10 10:41:28 +0200

    Update telegram.js

--- inicializacion_eventos/telegram.js
+++ inicializacion_eventos/telegram.js
@@ -1,88 +1,127 @@
 const { TelegramClient } = require("telegram");
 const { StoreSession } = require("telegram/sessions");
+const fs = require("fs").promises;
 const storeSession = new StoreSession("./inicializacion_eventos/telegram/");
 const Codigo = require("../schemas/codigo.js");
 const Codigo_ant = require("../schemas/codigo_antiguo.js");
-
+const Ofertas = require("../schemas/ofertas.js");
 // Función para esperar hasta que un nuevo código sea diferente o esté disponible
+
 const waitForNewCode = async () => {
-    let nuevoCodigo = null;
+  let nuevoCodigo = null;
 
-    // Bucle que espera hasta que un código esté disponible
-    while (true) {
-        await new Promise(resolve => setTimeout(resolve, 10000));  // Espera 5 segundos
-        nuevoCodigo = await Codigo.findOne({});
+  // Bucle que espera hasta que un código esté disponible
+  while (true) {
+    await new Promise((resolve) => setTimeout(resolve, 10000)); // Espera 5 segundos
+    nuevoCodigo = await Codigo.findOne({});
 
-        // Si el código existe, rompe el bucle
-        if (nuevoCodigo && nuevoCodigo.code) {
-            console.log("Nuevo código encontrado:", nuevoCodigo.code);
-            break
-        }
+    // Si el código existe, rompe el bucle
+    if (nuevoCodigo && nuevoCodigo.code) {
+      console.log("Nuevo código encontrado:", nuevoCodigo.code);
+      break;
     }
+  }
 
-    return nuevoCodigo;
+  return nuevoCodigo;
 };
 
 const telegramClientInit = async () => {
-    console.log("Iniciando sesión en Telegram, esperando el código de verificación...");
+  console.log(
+    "Iniciando sesión en Telegram, esperando el código de verificación..."
+  );
 
-    // Inicializar el cliente de Telegram
-    const telegram = new TelegramClient(storeSession, 20285890, process.env.API_HASH_TEL, {
-        connectionRetries: 5,
-    });
+  // Inicializar el cliente de Telegram
+  const telegram = new TelegramClient(
+    storeSession,
+    20285890,
+    process.env.API_HASH_TEL,
+    {
+      connectionRetries: 5,
+    }
+  );
 
-    await telegram.start({
-        phoneNumber: async () => process.env.NUM_TEL,
-        password: async () => process.env.PASSWORD_TEL,
+  await telegram.start({
+    phoneNumber: async () => process.env.NUM_TEL,
+    password: async () => process.env.PASSWORD_TEL,
 
-        // Esta función se llamará cuando Telegram pida el código de verificación
-        phoneCode: async () => {
-            console.log("Esperando que llegue el nuevo código...");
-            // Esperar hasta que el código esté disponible en la base de datos
-            const existeCodigo = await waitForNewCode();
-            const codeFromUser = String(existeCodigo.code);  // Asegúrate de que sea una cadena
+    // Esta función se llamará cuando Telegram pida el código de verificación
+    phoneCode: async () => {
+      console.log("Esperando que llegue el nuevo código...");
+      // Esperar hasta que el código esté disponible en la base de datos
+      const existeCodigo = await waitForNewCode();
+      const codeFromUser = String(existeCodigo.code); // Asegúrate de que sea una cadena
 
-            // Guardar el código anterior en la base de datos Codigo_ant
-            const nuevoCodigoAntiguo = new Codigo_ant({
-                code_ant: codeFromUser, // Guardar el código actual como "code_ant"
-            });
-            await nuevoCodigoAntiguo.save();
+      // Guardar el código anterior en la base de datos Codigo_ant
+      const nuevoCodigoAntiguo = new Codigo_ant({
+        code_ant: codeFromUser, // Guardar el código actual como "code_ant"
+      });
+      await nuevoCodigoAntiguo.save();
 
-            // Actualizar el código en la base de datos Codigo con el nuevo código
-            await Codigo.findOneAndUpdate(
-                { _id: existeCodigo._id }, // Buscar el código actual por su ID
-                { $set: { code: codeFromUser } },  // Establecer el nuevo código
-                { new: true }
-            );
+      // Actualizar el código en la base de datos Codigo con el nuevo código
+      await Codigo.findOneAndUpdate(
+        { _id: existeCodigo._id }, // Buscar el código actual por su ID
+        { $set: { code: codeFromUser } }, // Establecer el nuevo código
+        { new: true }
+      );
 
-            return codeFromUser;  // Devolver el código de verificación para Telegram
-        },
+      return codeFromUser; // Devolver el código de verificación para Telegram
+    },
 
+    //phoneCode: async () => input.text("Escribe el código de verificación: "),
+    onError: async (e) => console.log(e),
+  });
 
-        //phoneCode: async () => input.text("Escribe el código de verificación: "),
-        onError: async (e) => console.log(e)
-    });
+  console.log(
+    "Servicio de Telegram iniciado: " + (await telegram.getMe()).username
+  );
 
-    console.log('Servicio de Telegram iniciado: ' + (await telegram.getMe()).username);
-
-    return telegram;
+  return telegram;
 };
 
-// Función para buscar mensajes en un grupo de Telegram
 const busquedaMensaje = async (telegram) => {
-    const groupId = '@OfertasSensatas';
-    const mensaje = await telegram.getMessages(groupId, { limit: 5 });
-    const mensajeFiltrado = mensaje.filter(m => 
-        m.className === 'Message' && 
-        m.classType === 'constructor' && 
-        m.message && m.message.length > 0
-    );
-    const mensajeMasReciente = mensajeFiltrado.sort((a, b) => b.date - a.date)[0];
+  const groupId = "@OfertasSensatas";
 
-    return mensajeMasReciente;
+  // Obtener los últimos 5 mensajes
+  const mensajes = await telegram.getMessages(groupId, { limit: 5 });
+  // Filtrar los mensajes que son válidos (clase Message y contienen texto)
+  const mensajeFiltrado = mensajes.filter(
+    (m) =>
+      m.className === "Message" &&
+      m.classType === "constructor" &&
+      m.message &&
+      m.message.length > 0
+  );
+
+  // Obtener el mensaje más reciente
+  const mensajeMasReciente = mensajeFiltrado.sort((a, b) => b.date - a.date)[0];
+
+  if (!mensajeMasReciente) {
+    console.log("No se encontraron mensajes recientes.");
+    return;
+  }
+
+  // Verificar si el mensaje más reciente contiene una imagen
+  if (mensajeMasReciente.media && mensajeMasReciente.media.photo) {
+    try {
+      // Descargar el archivo de la imagen usando `downloadMedia`
+      const archivoBytes = await telegram.downloadMedia(
+        mensajeMasReciente.media
+      );
+
+      // Guardar la imagen en el sistema de archivos de manera asíncrona
+      await fs.writeFile(
+        "./inicializacion_eventos/imagenes/imagenOferta.jpg",
+        archivoBytes
+      );
+    } catch (err) {
+      console.error("Error al descargar la imagen:", err);
+    }
+  }
+
+  return mensajeMasReciente;
 };
 
 module.exports = {
-    telegramClientInit,
-    busquedaMensaje,
+  telegramClientInit,
+  busquedaMensaje,
 };
