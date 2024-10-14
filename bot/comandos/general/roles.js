b788b9481c753046b43404191ee0c4a23aa9feae
PARJOAC <133354750+PARJOAC@users.noreply.github.com>
2024-10-14 10:23:15 +0200

    Update roles.js

--- bot/comandos/general/roles.js
+++ bot/comandos/general/roles.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const boton = require("../../../extras/boton.js")
 const embed = require("../../../extras/embeds.js");
 
@@ -8,20 +8,15 @@ module.exports = {
       .setDescription("Enviar mensaje de autoroles"),
     category: "mod",
     usage: "Enviar mensaje de autoroles",
-    n: "</roles:1295292737184333864>",
+    n: "</roles:1276173928368181355>",
     async execute(interaction, client) {
-        try {
-            if(interaction.member.id !== "714376484139040809") return interaction.reply({ content: "No tienes permisos para ejecutar este comando.", ephemeral: true });
+
+        if(interaction.member.id !== "714376484139040809") return interaction.reply({ content: "No tienes permisos para ejecutar este comando.", ephemeral: true });
 
         await interaction.channel.send({ embeds: [embed.categorias_imagen()], components: [boton.botones_categorias()], ephemeral: false });
-        await interaction.channel.send({ embeds: [embed.general_3()], components: [boton.botones_curso()], ephemeral: false });
-        await interaction.channel.send({ embeds: [embed.general_3()], components: [boton.botones_comunidad()], ephemeral: false });
-        await interaction.channel.send({ embeds: [embed.general_3()], components: [boton.botones_genero()], ephemeral: false });
+        await interaction.channel.send({ embeds: [embed.curso()], components: [boton.botones_curso()], ephemeral: false });
+        await interaction.channel.send({ embeds: [embed.comunidad()], components: [boton.botones_comunidad()], ephemeral: false });
+        await interaction.channel.send({ embeds: [embed.genero()], components: [boton.botones_genero()], ephemeral: false });
         await interaction.channel.send({ embeds: [embed.minecraft()], components: [boton.botones_minecraft()], ephemeral: false });
-        } catch(e) {
-            console.log(e);
-        }
-
-        
     }
 }
