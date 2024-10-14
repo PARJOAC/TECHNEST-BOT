acdc47a5c6ca7a70fa8067796c41666159573e3b
PARJOAC <133354750+PARJOAC@users.noreply.github.com>
2024-10-14 09:45:03 +0200

    Add files via upload

---
+++ bot/comandos/general/roles.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const boton = require("../../extras/boton.js");
+const embed = require("../../extras/embeds.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("roles")
+      .setDescription("Enviar mensaje de autoroles"),
+    category: "mod",
+    usage: "Enviar mensaje de autoroles",
+    n: "</roles:1276173928368181355>",
+    async execute(interaction, client) {
+
+        if(interaction.member.id !== "714376484139040809") return interaction.reply({ content: "No tienes permisos para ejecutar este comando.", ephemeral: true });
+
+        await interaction.channel.send({ embeds: [embed.categorias_imagen()], components: [boton.botones_categorias()], ephemeral: false });
+        await interaction.channel.send({ embeds: [embed.general_3()], components: [boton.botones_curso()], ephemeral: false });
+        await interaction.channel.send({ embeds: [embed.general_3()], components: [boton.botones_comunidad()], ephemeral: false });
+        await interaction.channel.send({ embeds: [embed.general_3()], components: [boton.botones_genero()], ephemeral: false });
+        await interaction.channel.send({ embeds: [embed.minecraft()], components: [boton.botones_minecraft()], ephemeral: false });
+    }
+}
