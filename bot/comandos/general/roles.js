21cf197ebdf01ecfca450cdd32e5e4baa88d4ae6
PARJOAC <133354750+PARJOAC@users.noreply.github.com>
2024-10-14 19:11:17 +0200

    Update roles.js

--- bot/comandos/general/roles.js
+++ bot/comandos/general/roles.js
@@ -1,28 +1,44 @@
 const { SlashCommandBuilder } = require("discord.js");
-const boton = require("../../../extras/boton.js")
+const boton = require("../../../extras/boton.js");
 const embed = require("../../../extras/embeds.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-      .setName("roles")
-      .setDescription("Enviar mensaje de autoroles"),
-    category: "mod",
-    usage: "Enviar mensaje de autoroles",
-    n: "</roles:1276173928368181355>",
-    async execute(interaction, client) {
+  data: new SlashCommandBuilder()
+    .setName("roles")
+    .setDescription("Enviar mensaje de autoroles"),
+  category: "mod",
+  usage: "Enviar mensaje de autoroles",
+  n: "</roles:1276173928368181355>",
+  async execute(interaction, client) {
+    if (interaction.user.id !== "714376484139040809") return;
+    const categoriasMsg = await interaction.channel.send({
+      embeds: [embed.categorias_imagen()],
+      components: [boton.botones_categorias1(), boton.botones_categorias2()],
+    });
 
-        if(interaction.member.id !== "714376484139040809") return interaction.reply({ content: "No tienes permisos para ejecutar este comando.", ephemeral: true });
+    const cursoMsg = await interaction.channel.send({
+      embeds: [embed.curso()],
+      components: [boton.botones_curso()],
+    });
 
-        await interaction.channel.send({ embeds: [embed.categorias_imagen()], components: [boton.botones_categorias1(), boton.botones_categorias2()], ephemeral: false })
-        
-        await interaction.channel.send({ embeds: [embed.curso()], components: [boton.botones_curso()], ephemeral: false });
-        
-        await interaction.channel.send({ embeds: [embed.genero()], components: [boton.botones_genero()], ephemeral: false });
-        
-        await interaction.channel.send({ embeds: [embed.minecraft()], components: [boton.botones_minecraft()], ephemeral: false });
+    const comunidadsMsg = await interaction.channel.send({
+      embeds: [embed.comunidad()],
+      components: [
+        boton.botones_comunidad1(),
+        boton.botones_comunidad2(),
+        boton.botones_comunidad3(),
+        boton.botones_comunidad4(),
+      ],
+    });
 
-       // await interaction.channel.send({ embeds: [embed.comunidad()], components: [boton.botones_comunidad()], ephemeral: false });
-        
-        
-    }
-}
+    const generoMsg = await interaction.channel.send({
+      embeds: [embed.genero()],
+      components: [boton.botones_genero()],
+    });
+
+    const minecraftMsg = await interaction.channel.send({
+      embeds: [embed.minecraft()],
+      components: [boton.botones_minecraft()],
+    });
+  },
+};
