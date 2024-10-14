2231a3e4d9950a869d50c2422fa5b24992921f9c
PARJOAC <133354750+PARJOAC@users.noreply.github.com>
2024-10-14 12:02:28 +0200

    Actualizar interactionCreate.js

--- bot/eventos/interactionCreate.js
+++ bot/eventos/interactionCreate.js
@@ -31,7 +31,7 @@ module.exports = {
 
     if (interaction.isButton() && interaction.customId == "SMR") {
       const rol = interaction.guild.roles.cache.get("1107572341644480562");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -50,7 +50,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "TELECO") {
       const rol = interaction.guild.roles.cache.get("1107572397588107294");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -69,7 +69,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "ASIR") {
       const rol = interaction.guild.roles.cache.get("1107572531122155520");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -88,7 +88,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "DAW") {
       const rol = interaction.guild.roles.cache.get("1107572594552619038");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -107,7 +107,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "DAM") {
       const rol = interaction.guild.roles.cache.get("1107572573052600390");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -126,7 +126,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "CIBERSEGURIDAD") {
       const rol = interaction.guild.roles.cache.get("1107658851420999832");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -145,7 +145,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "IABG") {
       const rol = interaction.guild.roles.cache.get("1249719462731452506");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -164,7 +164,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "VR") {
       const rol = interaction.guild.roles.cache.get("1249720896386502689");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -183,7 +183,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "AUDIOVISUALES") {
       const rol = interaction.guild.roles.cache.get("1107573065832992798");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -203,7 +203,7 @@ module.exports = {
 
     if (interaction.isButton() && interaction.customId == "PRESENCIAL") {
       const rol = interaction.guild.roles.cache.get("1268294556558819561");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -222,7 +222,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "SEMIPRESENCIAL") {
       const rol = interaction.guild.roles.cache.get("1268294590528622755");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -241,7 +241,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "ONLINE") {
       const rol = interaction.guild.roles.cache.get("1268294630500466860");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -261,7 +261,7 @@ module.exports = {
 /*
     if (interaction.isButton() && interaction.customId == "ANDALUCIA") {
       const rol = interaction.guild.roles.cache.get("1267939687436255297");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -280,7 +280,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "CANARIAS") {
       const rol = interaction.guild.roles.cache.get("1267939748748460101");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -299,7 +299,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "CANTABRIA") {
       const rol = interaction.guild.roles.cache.get("1267939783569576001");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -318,7 +318,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "CATALUNA") {
       const rol = interaction.guild.roles.cache.get("1267939818235629578");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -337,7 +337,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "COMUNIDAD_VALENCIANA") {
       const rol = interaction.guild.roles.cache.get("1267940230984368290");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -356,7 +356,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "GALICIA") {
       const rol = interaction.guild.roles.cache.get("1267939847058882661");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -375,7 +375,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "ISLAS_BALEARES") {
       const rol = interaction.guild.roles.cache.get("1267939875294937139");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -394,7 +394,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "LA_RIOJA") {
       const rol = interaction.guild.roles.cache.get("1267939917905002642");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -413,7 +413,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "NAVARRA") {
       const rol = interaction.guild.roles.cache.get("1267939952994549780");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -432,7 +432,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "PAIS_VASCO") {
       const rol = interaction.guild.roles.cache.get("1267939988398669877");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -451,7 +451,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "ARAGON") {
       const rol = interaction.guild.roles.cache.get("1267940074562260992");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -470,7 +470,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "CASTILLA_LA_MANCHA") {
       const rol = interaction.guild.roles.cache.get("1267940126122573874");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -489,7 +489,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "CASTILLA_LEON") {
       const rol = interaction.guild.roles.cache.get("1267940186164301854");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -508,7 +508,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "MADRID") {
       const rol = interaction.guild.roles.cache.get("1267940326979403776");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -527,7 +527,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "EXTREMADURA") {
       const rol = interaction.guild.roles.cache.get("1267940277973422111");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -546,7 +546,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "CEUTA") {
       const rol = interaction.guild.roles.cache.get("1267940354750025832");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -565,7 +565,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "MELILLA") {
       const rol = interaction.guild.roles.cache.get("1267940385703985154");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -584,7 +584,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "MURCIA") {
       const rol = interaction.guild.roles.cache.get("1267940414892015746");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -603,7 +603,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "ASTURIAS") {
       const rol = interaction.guild.roles.cache.get("1267940446328459354");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -623,7 +623,7 @@ module.exports = {
 */
     if (interaction.isButton() && interaction.customId == "HOMBRE") {
       const rol = interaction.guild.roles.cache.get("1277609733884477460");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -642,7 +642,7 @@ module.exports = {
     }
     if (interaction.isButton() && interaction.customId == "MUJER") {
       const rol = interaction.guild.roles.cache.get("1277609779535151239");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
@@ -662,7 +662,7 @@ module.exports = {
 
     if (interaction.isButton() && interaction.customId == "MINECRAFT") {
       const rol = interaction.guild.roles.cache.get("1280265326168244255");
-      if (interaction.member.roles.cache.has(rol)) {
+      if (interaction.member.roles.cache.has(rol.id)) {
         interaction.member.roles.remove(rol);
         await interaction.reply({
           content: "¡Se te ha eliminado el rol!",
