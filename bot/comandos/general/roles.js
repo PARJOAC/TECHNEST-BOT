d2480226fadb1957e2f4d694e5bffd8791ef0694
PARJOAC <133354750+PARJOAC@users.noreply.github.com>
2024-10-14 14:52:30 +0200

    Update roles.js

--- bot/comandos/general/roles.js
+++ bot/comandos/general/roles.js
@@ -13,7 +13,7 @@ module.exports = {
 
         if(interaction.member.id !== "714376484139040809") return interaction.reply({ content: "No tienes permisos para ejecutar este comando.", ephemeral: true });
 
-        await interaction.channel.send({ embeds: [embed.categorias_imagen()], components: [boton.botones_categorias1(), boton.botones_categorias2()], ephemeral: false });
+        await interaction.channel.send({ embeds: [embed.categorias_imagen()], components: [boton.botones_categorias1(), boton.botones_categorias2()], ephemeral: false })
         .then(async() =>
             await interaction.channel.send({ embeds: [embed.curso()], components: [boton.botones_curso()], ephemeral: false });
         ).then(async() =>
